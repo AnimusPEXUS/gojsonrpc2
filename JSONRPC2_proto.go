b099e0ddf71e93411b90a1ca04088d833989a19d
AnimusPEXUS <animuspexusagutilities@gmail.com>
2023-04-20 01:46:28 +0300

    added JSON-RPC 2 error constant codes

--- JSONRPC2_proto.go
+++ JSONRPC2_proto.go
@@ -302,3 +302,25 @@ func (self *Message) IsError() bool {
 
 	return !self.HasResultField() && self.HasErrorField()
 }
+
+// https://www.jsonrpc.org/specification#error_object
+// code 	message 	meaning
+// -32700 	Parse error 	Invalid JSON was received by the server.
+// An error occurred on the server while parsing the JSON text.
+// -32600 	Invalid Request 	The JSON sent is not a valid Request object.
+// -32601 	Method not found 	The method does not exist / is not available.
+// -32602 	Invalid params 	Invalid method parameter(s).
+// -32603 	Internal error 	Internal JSON-RPC error.
+// -32000 to -32099 	Server error 	Reserved for implementation-defined server-errors.
+
+type ProtocolErrorConst int
+
+const (
+	ProtocolErrorConstParseError     ProtocolErrorConst = -32700
+	ProtocolErrorConstInvalidRequest ProtocolErrorConst = -32600
+	ProtocolErrorMethodNotFound      ProtocolErrorConst = -32601
+	ProtocolErrorInvalidParams       ProtocolErrorConst = -32602
+	ProtocolErrorInternalError       ProtocolErrorConst = -32603
+	ProtocolErrorServerError_32000   ProtocolErrorConst = -32000
+	ProtocolErrorServerError_32099   ProtocolErrorConst = -32099
+)
